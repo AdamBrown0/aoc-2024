a12d69c784b4208fbb9c33979ee75d3daa0226ea
AdamBrown0 <adambrown0@users.noreply.github.com>
2024-12-05 11:51:29 +0000

    Day 5

--- README.md
+++ README.md
@@ -19,11 +19,13 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
-| [Day 1](./src/bin/01.rs) | `62.8µs` | `74.0µs` |
-| [Day 2](./src/bin/02.rs) | `84.3µs` | `216.4µs` |
-| [Day 3](./src/bin/03.rs) | `189.7µs` | `673.1µs` |
+| [Day 1](./src/bin/01.rs) | `120.0µs` | `233.0µs` |
+| [Day 2](./src/bin/02.rs) | `193.1µs` | `508.4µs` |
+| [Day 3](./src/bin/03.rs) | `362.2µs` | `2.8ms` |
+| [Day 4](./src/bin/04.rs) | `-` | `-` |
+| [Day 5](./src/bin/05.rs) | `-` | `-` |
 
-**Total: 1.30ms**
+**Total: 4.22ms**
 <!--- benchmarking table --->
 
 ---
---
+++ src/bin/05.rs
@@ -0,0 +1,33 @@
+use std::io::Lines;
+
+advent_of_code::solution!(5);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let orderings = input.lines().take(1176);
+    let page_numbers = input.lines().skip(1177);
+    for line in page_numbers {
+        let _ = line.split(',').skip(1).map(); // each value runs through orderings n
+    }
+    None
+}
+
+pub fn part_two(_input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(143));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
